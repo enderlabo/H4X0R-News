3fded10884c9ffa46b7e5e2d95a9e5f84fb0e57c
dev_ElderLab <enderlabo@gmail.com>
2021-07-12 10:37:43 -0500

    Initial Commit

---
+++ H4x0r News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4x0r News
+//
+//  Created by Laborit on 12/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4x0r News/H4x0r_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4x0r_NewsApp.swift
+//  H4x0r News
+//
+//  Created by Laborit on 12/07/21.
+//
+
+import SwiftUI
+
+@main
+struct H4x0r_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
